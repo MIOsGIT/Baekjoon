69a5fb92cf0726c45181a1cdbdb9638bd0b1d512
MIO <168819920+MIOsGIT@users.noreply.github.com>
2024-05-06 20:39:11 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+        int j = 0;
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++) {
+        j += i;
+    }
+    printf("%d", j);
+    return 0;
+}
