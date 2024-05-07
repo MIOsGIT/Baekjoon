333019f35ff3ee22563285316fec0fd67a0fc79f
MIO <168819920+MIOsGIT@users.noreply.github.com>
2024-05-07 10:34:09 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int k = 0; k <= n - i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
