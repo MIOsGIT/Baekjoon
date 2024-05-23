b014f23302c625bb0311e67c9d26f4dffb287485
MIO <168819920+MIOsGIT@users.noreply.github.com>
2024-05-23 19:14:33 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cc
+++ 백준/Bronze/8393. 합/합.cc
@@ -1,14 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#pragma warning(disable : 4996)
+#include<stdio.h>
 
-int main(void)
-{
-    int n;
-        int j = 0;
-    scanf("%d", &n);
-    for (int i = 0; i <= n; i++) {
-        j += i;
-    }
-    printf("%d", j);
-    return 0;
+int main() {
+
+	int n, result=0 ;
+
+	scanf("%d", &n);
+
+	for (int i = 0; n >= i; i++) {
+		result = result + i;
+	}
+
+	printf("%d", result);
+
+	return 0;
 }
