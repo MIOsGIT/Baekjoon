cb48c67e75c860a892e53adc6e5eb1055bff7901
MIO <168819920+MIOsGIT@users.noreply.github.com>
2024-06-10 10:16:25 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	if (n == 0) {
+		printf("YONSEI");
+	}
+	else if (n=1) printf("Leading the Way to the Future");
+}
